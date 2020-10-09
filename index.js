f269f18e35cb33ee0cb7c8fd247fe320519f61da
Migs Ladisla <migsladisla@gmail.com>
2020-10-09 19:24:32 +0800

    Winston logger configuration and implementation to all console logs and API error messages

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 'use strict';
 
+const express = require('express');
+const app = express();
+const port = 8010;
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.json');
-const port = 8010;
+const logger = require('./src/logger');
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
@@ -10,14 +17,11 @@ const db = new sqlite3.Database(':memory:');
 const buildSchemas = require('./src/schemas');
 
 db.serialize(() => {
-	buildSchemas(db);
+    buildSchemas(db);
 
-	const app = require('./src/app')(db);
-
-	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-	app.listen(port, () =>
-		// eslint-disable-next-line no-undef
-		console.log(`App started and listening on port ${port}`)
-	);
+    const app = require('./src/app')(db);
+    
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+    
+    app.listen(port, () => logger.log('info', `App started and listening on port ${port}`));
 });
